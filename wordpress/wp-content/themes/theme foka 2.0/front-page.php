bb59e56e4f16da2c6240e39542c0246fb07d3487
adrian <adrian.miranda.r@gmail.com>
2019-09-04 23:28:17 -0300

    recoendados funcionando solo falta hacerlo flex

--- wordpress/wp-content/themes/theme foka 2.0/front-page.php
+++ wordpress/wp-content/themes/theme foka 2.0/front-page.php
@@ -47,15 +47,39 @@
 
 <!-- seccion 2 -->
       <div class="col-lg-9 contenedorRecomendados">
-        
-        
         <a href="recomendados">
           <h3 class="mt-3" style="text-align: center;">Recomendados de foca</h3>
         </a>
         <hr>
-          <?php 
+        <?php 
+        // The Query
+        $the_query = new WP_Query( array( 'posts_per_page' => 9,  'category_name' => 'recomendados'));
+        // The Loop
+        if ( $the_query->have_posts() ) : ?>    
+          <?php while ( $the_query->have_posts() ) :
+                        $the_query->the_post(); ?>
+            <a href="<?php the_permalink() ?>">
+                    <?php if ( has_post_thumbnail() ) 
+                      {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid recomendado '));
+                      }
+                      ?>
+              </a>
+                  <?php endwhile ?>
+            <?php wp_reset_postdata();
+                    else : ?>
+                        <p>no posts found </p> 
+                <?php endif; ?>
+              </div>
+
+        <div class=" col-lg-12 reportajes">
+              <a href="reportajes">
+            <h3>Historio del</h3>
+            </a>
+            <hr>
+            <div class="historia col-lg-3">
+            <?php 
           // The Query
-          $the_query = new WP_Query( array( 'posts_per_page' => 9,  'category_name' => 'recomendados'));
+          $the_query = new WP_Query( array( 'posts_per_page' => 3,  'category_name' => 'reportajes'));
           // The Loop
           if ( $the_query->have_posts() ) : ?>    
             <?php while ( $the_query->have_posts() ) :
@@ -63,10 +87,14 @@
               
               
                 <a href="<?php the_permalink() ?>">
-                  <?php if ( has_post_thumbnail() ) 
-                        {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid recomendado '));
+                <p><?php the_title()?></p>
+                <?php if ( has_post_thumbnail() ) 
+                        {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid '));
                         }
                         ?>
+              <p style="margin: 10px;"><?php the_excerpt() ?></p>
+              <button class="btn btn-block"><p style="text-align: center;">
+                <a href="<?php the_permalink() ?>">click aca</a></p></button>
                 </a>
                       
         
@@ -75,22 +103,7 @@
                     else : ?>
                         <p>no posts found </p> 
                 <?php endif; ?>  
-              
-           
-
-            
-                  
-          </div>
-          <div class=" padre col-lg-12">
-              <a href="reportajes">
-            <h3>Historio del</h3>
-            </a>
-            <hr>
-            <div class="historia col-lg-3">
-              <img src="<?php bloginfo('template_url')?>/adr.png" alt="" class="img-fluid">
-              <p style="margin: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ea blanditiis. Aspernatur error, architecto, optio illo deleniti nam vel maxime sunt et aliquam numquam dignissimos omnis asperiores dicta voluptas cumque!</p>
-              <button class="btn btn-block"><p style="text-align: center;">
-                <a href="single.html">click aca</a></p></button>
+                
             </div>
            
           </div>
