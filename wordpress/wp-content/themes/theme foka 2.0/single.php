06b2b7133588edd07b5aa12da83548cd580e6c8a
adrian <adrian.miranda.r@gmail.com>
2019-09-06 00:35:27 -0300

    single creada

--- wordpress/wp-content/themes/theme foka 2.0/single.php
+++ wordpress/wp-content/themes/theme foka 2.0/single.php
@@ -1,42 +1,33 @@
 <?php get_header() ?>
-<div class="col-lg-9 contenedorRecomendados">
-        
-        
-        <a href="recomendados">
-          <h3 class="mt-3" style="text-align: center;">Recomendados de foca</h3>
-        </a>
-        <hr>
-          <?php 
-          // The Query
-          $the_query = new WP_Query( array( 'posts_per_page' => 9,  'category_name' => 'recomendados'));
-          // The Loop
-          if ( $the_query->have_posts() ) : ?>    
-            <?php while ( $the_query->have_posts() ) :
-                          $the_query->the_post(); ?>
-              
-              
-                <a href="<?php the_permalink() ?>">
-                  <?php if ( has_post_thumbnail() ) 
-                        {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid recomendado '));
-                        }
-                        ?>
-                </a>
-                      
-        
-                <?php endwhile ?>
-            <?php wp_reset_postdata();
-                    else : ?>
-                        <p>no posts found </p> 
-                <?php endif; ?>  
+<div class="row">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-10">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+             
+                <div class="card-body mb-auto elpost">
+                <h3>
+                        <?php the_title() ?>
+                    </h3>
+                    <?php if ( has_post_thumbnail() ) {
+                    the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid mb-3 single'));
+                    }?>
+                   
+                <p>
+                    <?php the_content() ?>
+                </p>
+               
 
 
-                <!-- reportajes -->
-
-                
-              
-           
+                    <a href="javascript:history.back()"> 
+                    <input type="button" value=”Volver  onclick=”history.back(-1)” class="btn  btn-block"/></a>
 
             
-             <hr>     
-          </div>
+
+
+                </div>
+            </div>
+       <?php endwhile; endif; ?>
+    </div>
+    <div class="col-lg-1"></div>
+</div>
 <?php get_footer() ?>
