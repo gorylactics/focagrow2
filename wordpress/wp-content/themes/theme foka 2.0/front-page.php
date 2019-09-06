aa5424398f096189da2c37f368d7b47701b868e9
adrian <adrian.miranda.r@gmail.com>
2019-09-06 00:24:55 -0300

    semillas sin card aun

--- wordpress/wp-content/themes/theme foka 2.0/front-page.php
+++ wordpress/wp-content/themes/theme foka 2.0/front-page.php
@@ -47,8 +47,6 @@
 
 <!-- seccion 2 -->
       <div class="col-lg-9 contenedorRecomendados">
-        
-        
         <a href="recomendados">
           <h3 class="mt-3" style="text-align: center;">Recomendados de foca</h3>
         </a>
@@ -60,32 +58,24 @@
           if ( $the_query->have_posts() ) : ?>    
             <?php while ( $the_query->have_posts() ) :
                           $the_query->the_post(); ?>
-              
-              
+
                 <a href="<?php the_permalink() ?>">
                   <?php if ( has_post_thumbnail() ) 
                         {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid recomendado '));
                         }
                         ?>
                 </a>
-                      
-        
+
                 <?php endwhile ?>
             <?php wp_reset_postdata();
                     else : ?>
                         <p>no posts found </p> 
                 <?php endif; ?>  
 
-
-                <!-- reportajes -->
-
-                
-              
-           
-
-            
              <hr>     
           </div>
+          
+           <!-- reportajes -->
           <div class="col-lg-12">
           <a href="reportajes">
           <h3 class="mt-3" style="text-align: center;">Historias de una foca</h3>
--- wordpress/wp-content/themes/theme foka 2.0/page-semilla.php
+++ wordpress/wp-content/themes/theme foka 2.0/page-semilla.php
@@ -1,38 +1,38 @@
 <?php get_header(); ?>
-<div class="col-lg-9 contenedorRecomendados">
-    <h3 class="mt-3" style="text-align: center;">Semillas</h3>
-    <hr>
-    <?php 
-    // The Query
-    $the_query = new WP_Query( array( 'posts_per_page' => 9,  'category_name' => 'recomendados'));
-    // The Loop
-    if ( $the_query->have_posts() ) : ?>    
-        <?php while ( $the_query->have_posts() ) :
-                        $the_query->the_post(); ?>
-        <p><?php get_the_title()?></p>
-        <a href="<?php the_permalink() ?>">
-        
+    <div class="col-lg-12 contenedorRecomendados">
+        <div>    
+            <h3 class="mt-3" style="text-align: center;">Semillas
+            </h3>
+            <hr>
+        </div> 
+        <div>    
+            <h3 class="mt-3" style="text-align: center;">Sativas
+            </h3>
+            <hr>
+        </div>    
+          <?php 
+          // The Query
+          $the_query = new WP_Query( array( 'posts_per_page' => 9,  'category_name' => 'recomendados'));
+          // The Loop
+          if ( $the_query->have_posts() ) : ?>    
+            <?php while ( $the_query->have_posts() ) :
+                          $the_query->the_post(); ?>
+
+                <a href="<?php the_permalink() ?>">
                   <?php if ( has_post_thumbnail() ) 
                         {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid recomendado '));
                         }
                         ?>
                 </a>
-                      
-        
+
                 <?php endwhile ?>
             <?php wp_reset_postdata();
                     else : ?>
                         <p>no posts found </p> 
                 <?php endif; ?>  
 
-
-                <!-- reportajes -->
-
-                
-              
-           
-
-            
              <hr>     
           </div>
+          
+          
 <?php get_footer(); ?>
