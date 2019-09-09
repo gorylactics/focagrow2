89f41c26c41ef6ff77fac5d5567ad904013970e3
adrian <adrian.miranda.r@gmail.com>
2019-09-08 21:25:47 -0300

    creacion de categorias

--- wordpress/wp-content/themes/theme foka 2.0/page-semilla.php
+++ wordpress/wp-content/themes/theme foka 2.0/page-semilla.php
@@ -1,38 +1,66 @@
 <?php get_header(); ?>
+<div class="row">
     <div class="col-lg-12 contenedorRecomendados">
-        <div>    
+        <div class="col-lg-12">    
             <h3 class="mt-3" style="text-align: center;">Semillas
             </h3>
             <hr>
         </div> 
-        <div>    
-            <h3 class="mt-3" style="text-align: center;">Sativas
+        <div class="col-lg-12">    
+            <h3 class="mt-3" style="text-align: center;">Sativas Feminizadas
             </h3>
             <hr>
-        </div>    
+        </div> 
+        <div class="col-lg-12">   
           <?php 
           // The Query
-          $the_query = new WP_Query( array( 'posts_per_page' => 9,  'category_name' => 'recomendados'));
+          $the_query = new WP_Query( array( 'posts_per_page' => 6,  'category_name' => 'sativas-feminizadas'));
           // The Loop
           if ( $the_query->have_posts() ) : ?>    
             <?php while ( $the_query->have_posts() ) :
                           $the_query->the_post(); ?>
-
                 <a href="<?php the_permalink() ?>">
                   <?php if ( has_post_thumbnail() ) 
                         {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid recomendado '));
                         }
-                        ?>
+                    ?>
                 </a>
-
-                <?php endwhile ?>
+            <?php endwhile ?>
             <?php wp_reset_postdata();
                     else : ?>
                         <p>no posts found </p> 
-                <?php endif; ?>  
+                <?php endif; ?>
+            <hr>  
+        </div>
+        <div class="col-lg-12">    
+            <h3 class="mt-3" style="text-align: center;">Sativas Automaticas
+            </h3>
+            <hr>
+        </div> 
+        <div class="col-lg-12">   
+          <?php 
+          // The Query
+          $the_query = new WP_Query( array( 'posts_per_page' => 9,  'category_name' => 'sativas-automaticas'));
+          // The Loop
+          if ( $the_query->have_posts() ) : ?>    
+            <?php while ( $the_query->have_posts() ) :
+                          $the_query->the_post(); ?>
+                <a href="<?php the_permalink() ?>">
+                  <?php if ( has_post_thumbnail() ) 
+                        {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid recomendado '));
+                        }
+                    ?>
+                </a>
+            <?php endwhile ?>
+            <?php wp_reset_postdata();
+                    else : ?>
+                        <p>no posts found </p> 
+                <?php endif; ?>
+            <hr>  
+        </div>      
+    </div>
+</div>
 
-             <hr>     
-          </div>
           
           
 <?php get_footer(); ?>
