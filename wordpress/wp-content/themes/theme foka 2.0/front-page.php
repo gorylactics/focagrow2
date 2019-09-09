2137b93332f19ce9d263a0d0890156e8c9c69ee6
adrian <adrian.miranda.r@gmail.com>
2019-09-09 00:33:16 -0300

    front page y productos ok

--- wordpress/wp-content/themes/theme foka 2.0/front-page.php
+++ wordpress/wp-content/themes/theme foka 2.0/front-page.php
@@ -4,9 +4,11 @@
 <div class="row">
 <!-- secciones -->
   <div class="col-lg-12">
+    <div class="col-lg-12">
     <h3 class="mt-3" style="text-align: center;">Secciones
     </h3>
     <hr>
+    </div>
   </div>
   <div class="container padre col-lg-9">
     <div class="card">
@@ -46,8 +48,9 @@
       <h3 class="mt-3" style="text-align: center;">Recomendados de foca</h3>
     </a>
     <hr>
+    <div>
     <?php
-      $the_query = new WP_Query( array( 'posts_per_page' => 9,  'category_name' => 'recomendados , reportajes'));
+      $the_query = new WP_Query( array( 'posts_per_page' => 9,  'category_name' => 'recomendados'));
         if ( $the_query->have_posts() ) : ?>    
       <?php while ( $the_query->have_posts() ) :
                   $the_query->the_post(); ?>
@@ -62,6 +65,7 @@
           else : ?>
           <p>no posts found </p> 
           <?php endif; ?>
+          </div>
     <hr>     
   </div>
           
@@ -71,25 +75,34 @@
     </a>
     <hr>
   </div>
-  <div class="col-lg-12 contenedorNET">
-    <?php 
-      $the_query = new WP_Query( array( 'posts_per_page' => 4,  'category_name' => 'reportajes'));
-      if ( $the_query->have_posts() ) : ?>    
-      <?php while ( $the_query->have_posts() ) :
-        $the_query->the_post(); ?>
-    <div class="historia col-lg-3">
-      <a href="<?php the_permalink() ?>">
-        <?php if ( has_post_thumbnail() ) 
-          {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid recomendados '));
-          }
-        ?>
-        <p><?php the_excerpt() ?></p>     
-  </div> 
-    <?php endwhile ?>
-      <?php wp_reset_postdata();
-      else : ?>
-        <p>no posts found </p> 
-      <?php endif; ?>  
+  
+    
+  <div class="col-lg-12 padreProducto">   
+          <?php 
+          // The Query
+          $the_query = new WP_Query( array( 'posts_per_page' => 4,  'category_name' => 'reportajes , recomendados'));
+          // The Loop
+          if ( $the_query->have_posts() ) : ?>    
+            <?php while ( $the_query->have_posts() ) :
+                          $the_query->the_post(); ?>
+            <div class="col-lg-3 historia">
+                <a href="<?php the_permalink() ?>">
+                <p><?php the_title() ?></p>
+                <hr>
+                  <?php if ( has_post_thumbnail() ) 
+                        {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid'));
+                        }
+                    ?><hr>
+                    <?php the_excerpt() ?>
+                </a>
+                </div>
+            <?php endwhile ?>
+            <?php wp_reset_postdata();
+                    else : ?>
+                        <p>no posts found </p> 
+                <?php endif; ?>
+            <hr>  
+        </div>
       </div>       
     </div>        
   </div>
@@ -97,5 +110,5 @@
 </div><!-- div principal cierre ROW del Contenido-->
 <!-- /contenido -->
 
-<?php get_footer(); ?>
+<?php get_footer() ?>
 
--- wordpress/wp-content/themes/theme foka 2.0/page-semilla.php
+++ wordpress/wp-content/themes/theme foka 2.0/page-semilla.php
@@ -11,21 +11,22 @@
             </h3>
             <hr>
         </div> 
-        <div class="col-lg-12">   
+        <div class="col-lg-12 padreProducto">   
           <?php 
           // The Query
-          $the_query = new WP_Query( array( 'posts_per_page' => 6,  'category_name' => 'reportajes'));
+          $the_query = new WP_Query( array( 'posts_per_page' => 6,  'category_name' => 'reportajes , recomendados'));
           // The Loop
           if ( $the_query->have_posts() ) : ?>    
             <?php while ( $the_query->have_posts() ) :
                           $the_query->the_post(); ?>
-            <div class="card col-lg-3">
+            <div class="card col-lg-2 productos">
                 <a href="<?php the_permalink() ?>">
                 <p><?php the_title() ?></p>
+                <hr>
                   <?php if ( has_post_thumbnail() ) 
-                        {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid  '));
+                        {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid'));
                         }
-                    ?>
+                    ?><hr>
                     <?php the_excerpt() ?>
                 </a>
                 </div>
@@ -36,32 +37,7 @@
                 <?php endif; ?>
             <hr>  
         </div>
-        <div class="col-lg-12">    
-            <h3 class="mt-3" style="text-align: center;">Sativas Automaticas
-            </h3>
-            <hr>
-        </div> 
-        <div class="col-lg-12">   
-          <?php 
-          // The Query
-          $the_query = new WP_Query( array( 'posts_per_page' => 9,  'category_name' => 'sativas-automaticas'));
-          // The Loop
-          if ( $the_query->have_posts() ) : ?>    
-            <?php while ( $the_query->have_posts() ) :
-                          $the_query->the_post(); ?>
-                <a href="<?php the_permalink() ?>">
-                  <?php if ( has_post_thumbnail() ) 
-                        {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid recomendado '));
-                        }
-                    ?>
-                </a>
-            <?php endwhile ?>
-            <?php wp_reset_postdata();
-                    else : ?>
-                        <p>no posts found </p> 
-                <?php endif; ?>
-            <hr>  
-        </div>      
+        
     </div>
 </div>
 
