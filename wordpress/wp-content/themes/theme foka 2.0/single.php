d4e1a9bf5be7783f2c10005e21fc14a5c4616254
adrian <adrian.miranda.r@gmail.com>
2019-09-12 00:35:13 -0300

    insercion de tag con precio y proxima creacion boton ver todo

--- wordpress/wp-content/themes/theme foka 2.0/single.php
+++ wordpress/wp-content/themes/theme foka 2.0/single.php
@@ -1,40 +1,26 @@
 <?php get_header() ?>
 <div class="row">
-    <div class="col-lg-1"></div>
-    <div class="col-lg-10">
+    <div class="col-lg-5">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-             
-                <div class="card-body mb-auto elpost">
-                <h3>
-                        <?php the_title() ?>
-                    </h3>
-                    <?php if ( has_post_thumbnail() ) {
-                    the_post_thumbnail('medium' , array('class' => 'img-fluid 
-                    '));
-                    }?>
-                   
-                <p>
-                    <?php the_content() ?>
-                </p>
-                <p>
-                    Valor:<?php the_tag() ?>
-                </p>
-
-               
-
-               
-
-
-                    <a href="javascript:history.back()"> 
-                    <input type="button" value=”Volver  onclick=”history.back(-1)” class="btn  btn-block"/>ver todo</a>
-
-            
-
-
-                </div>
-            </div>
+        <div class="card-body mb-auto">
+            <h3>
+            <?php the_title() ?>
+            </h3>
+            <?php if ( has_post_thumbnail() ) {
+            the_post_thumbnail('medium' , array('class' => 'img-fluid'));
+            }?>
+            <p>
+                <?php the_content() ?>
+            </p>
+            <p>ver todo <?php the_category(' ') ?></p>
+            <p><?php the_tags('Precio = $ ') ?></p>
+            <a href="javascript:history.back()"> 
+                <input type="button" value=Volver  onclick=”history.back(-1)”/>
+            </a>
+        </div>    
        <?php endwhile; endif; ?>
-    </div>
-    <div class="col-lg-1"></div>
+       <div>
+       
+            </div>
 </div>
 <?php get_footer() ?>
